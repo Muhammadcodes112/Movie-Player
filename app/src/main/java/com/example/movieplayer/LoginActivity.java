e3e937cb60aecef7aa19fd2a87a2cf4704317042
Muhammadcodes112 <funuyallen@gmail.com>
2024-05-05 10:39:39 -0700

    first commit

---
+++ app/src/main/java/com/example/movieplayer/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.movieplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
